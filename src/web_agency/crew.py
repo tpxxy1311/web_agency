53a6063294e87f409f87eab95c6ec479efd42371
Tim Peters <timpetersohlsbach@gmail.com>
2025-04-18 17:58:42 +0200

    f

--- src/web_agency/crew.py
+++ src/web_agency/crew.py
@@ -304,7 +304,6 @@ class WebAgency():
 			agents=self.agents, # Automatically created by the @agent decorator
 			tasks=self.tasks, # Automatically created by the @task decorator
 			process=Process.sequential,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
 			verbose=True,
 		)
 
